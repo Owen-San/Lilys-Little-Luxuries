08e770395cde0432c1e708585c84aef821dbbd66
Owen <iamnytla@gmail.com>
2025-06-25 18:53:06 -0400

    Created and stylized the site. Awaiting client input on further stylization.

---
+++ index.js
@@ -0,0 +1,27 @@
+// FAQ Logic
+
+document.addEventListener("DOMContentLoaded", () => {
+  const faqItems = document.querySelectorAll(".faq__item");
+
+  faqItems.forEach((item) => {
+    const question = item.querySelector(".faq__question");
+
+    question.addEventListener("click", () => {
+      item.classList.toggle("active");
+
+      faqItems.forEach((i) => {
+        if (i !== item) i.classList.remove("active");
+      });
+    });
+  });
+});
+
+// BURGER MENU
+
+function openMenu() {
+  document.body.classList.add("menu--open");
+}
+
+function closeMenu() {
+  document.body.classList.remove("menu--open");
+}
